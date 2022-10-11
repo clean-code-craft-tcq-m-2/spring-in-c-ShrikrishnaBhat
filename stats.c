7954f4bb5f8b3fb034eccf8dd0580f3f260c2171
ShrikrishnaBhat <115543078+ShrikrishnaBhat@users.noreply.github.com>
2022-10-11 16:32:32 +0530

    Stat.c
    Code changed

--- stats.c
+++ stats.c
@@ -5,6 +5,8 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    
+    (void)setlength;
 }
 
 int emailAlertCallCount = 0;
