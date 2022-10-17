631e11c122e92f81c14c44b2d3c1522bc5c4296d
ShrikrishnaBhat <115543078+ShrikrishnaBhat@users.noreply.github.com>
2022-10-17 16:03:22 +0530

    Update Alert.c

--- Alert.c
+++ Alert.c
@@ -10,9 +10,3 @@ void ledAlerter()
 {
   ledAlertCallCount=1;
 }
-  /*alerter_funcptr alerters = &emailAlerter;
-  emailAlerter();
-  
-  //alerter_funcptr alerters = &ledAlerter;
-  ledAlerter();*/
-
