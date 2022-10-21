d4b60310a21b405cb49984255eb71014fef15214
ShrikrishnaBhat <115543078+ShrikrishnaBhat@users.noreply.github.com>
2022-10-21 15:56:48 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,23 +1,52 @@
 #include "stats.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
-    
-    (void)setlength;
-    (void)numberset;
-    
-    return s;
-}
-
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
-
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
-{
-    (void) maxThreshold;
-    (void) alerters;
-    (void) computedStats;
+struct Stats compute_statistics(const float* numberset, int setlength) {                
+    struct Stats s;                
+                    
+    float sum=0.0, avg=0.0, small=0.0, larger=0.0;                
+    int i=0;                
+    if(setlength==0)                
+    {                
+        s.average = NAN;                
+        s.min = NAN;                
+        s.max = NAN;                
+                    
+        return s;                
+    }                
+    else                
+    {                
+        for(i=0; i<setlength; i++)                
+        {                
+            sum=sum+numberset[i];                
+        }                
+                    
+        avg=sum/setlength;                
+                    
+        small=numberset[0];                
+        for(i=0; i<setlength; i++)                
+        {                
+            if(numberset[i]<small)                
+            {                
+             small=numberset[i];                
+            }                
+        }                
+                    
+        larger=numberset[0];                
+        for(i=0; i<setlength; i++)                
+        {                
+            if(numberset[i]>larger)                
+            {                
+                larger=numberset[i];                
+            }                
+        }                
+                    
+        //printf("AVG=%f, SMALL=%f, LARGER=%f", avg,small,larger);                
+                    
+        s.average = avg;                
+        s.min = small;                
+        s.max = larger;                
+                    
+        return s;                
+    }                
+                    
 }
