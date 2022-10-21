a0775f2be61ad124ab863921deea6aa513b08b1f
ShrikrishnaBhat <115543078+ShrikrishnaBhat@users.noreply.github.com>
2022-10-21 17:14:33 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -5,7 +5,7 @@
 
 struct Stats compute_statistics(const float* numberset, int setlength)
 {
-  struct Stats s;
+  struct Stats s;
   float sum=0.0, avg=0.0, small=0.0, larger=0.0;
     int i=0;
     if(setlength==0)
