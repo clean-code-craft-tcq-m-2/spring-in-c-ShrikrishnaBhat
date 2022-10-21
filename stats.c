4506f63cefcf3d9df1fd4eaf870e6c43ae6e52cf
ShrikrishnaBhat <115543078+ShrikrishnaBhat@users.noreply.github.com>
2022-10-21 17:11:27 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -3,7 +3,8 @@
 #include "stats.h"
 #include "Alert.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
+struct Stats compute_statistics(const float* numberset, int setlength)
+{
     struct Stats s;
     float sum=0.0, avg=0.0, small=0.0, larger=0.0;
     int i=0;
