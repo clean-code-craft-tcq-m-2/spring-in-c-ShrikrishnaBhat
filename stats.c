0dd62881448217f3e8eda52a3f40d407489d5ef3
ShrikrishnaBhat <115543078+ShrikrishnaBhat@users.noreply.github.com>
2022-10-21 16:16:10 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,75 +1,66 @@
 #include <stdio.h>
-#include "math.h"	             
-#include "stats.h"	         
+#include "math.h"
+#include "stats.h"
 #include "Alert.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {                
-    struct Stats s;                
+struct Stats compute_statistics(const float* numberset, int setlength) {
+    struct Stats s;
+    float sum=0.0, avg=0.0, small=0.0, larger=0.0;
+    int i=0;
+    if(setlength==0)
+    {
+        s.average = NAN;
+        s.min = NAN;
+        s.max = NAN;
                     
-    float sum=0.0, avg=0.0, small=0.0, larger=0.0;                
-    int i=0;                
-    if(setlength==0)                
-    {                
-        s.average = NAN;                
-        s.min = NAN;                
-        s.max = NAN;                
-                    
-        return s;                
-    }                
-    else                
-    {                
-        for(i=0; i<setlength; i++)                
-        {                
-            sum=sum+numberset[i];                
-        }                
-                    
-        avg=sum/setlength;                
-                    
-        small=numberset[0];                
-        for(i=0; i<setlength; i++)                
-        {                
-            if(numberset[i]<small)                
-            {                
-             small=numberset[i];                
-            }                
-        }                
-                    
-        larger=numberset[0];                
-        for(i=0; i<setlength; i++)                
-        {                
-            if(numberset[i]>larger)                
-            {                
-                larger=numberset[i];                
-            }                
-        }                
-                    
-        //printf("AVG=%f, SMALL=%f, LARGER=%f", avg,small,larger);                
-                    
-        s.average = avg;                
-        s.min = small;                
-        s.max = larger;                
-                    
-        return s;                
-    }                
-                    
+        return s;
+    }
+    else
+    {
+        for(i=0; i<setlength; i++)
+        {
+            sum=sum+numberset[i];
+        }
+        avg=sum/setlength;
+        small=numberset[0];
+        for(i=0; i<setlength; i++)
+        {
+            if(numberset[i]<small)
+            {
+             small=numberset[i];
+            }
+        }
+        larger=numberset[0];
+        for(i=0; i<setlength; i++)
+        {
+            if(numberset[i]>larger)
+            {
+                larger=numberset[i];
+            }
+        }
+        //printf("AVG=%f, SMALL=%f, LARGER=%f", avg,small,larger);
+        s.average = avg;
+        s.min = small;
+        s.max = larger;
+        return s;
+    }
 }
-
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[],struct Stats computedStats)                
-{                
-    if (computedStats.max > maxThreshold)                
-    {                
-        if(*alerters==emailAlerter)                
-        {                
-            emailAlerter();                
-        }                
-        if(*alerters==ledAlerter)                
-        {                
-           ledAlerter();                
-        }                
-    }                
-    else                
-    {               
-        emailAlertCallCount = 0;                
-        ledAlertCallCount = 0;                
-    }                                 
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[],struct Stats computedStats)
+{
+    if (computedStats.max > maxThreshold)
+    {
+        if(*alerters==emailAlerter)
+        {
+            emailAlerter();
+        }
+        if(*alerters==ledAlerter)
+        {
+           ledAlerter();
+        }
+    }
+    else
+    {
+        emailAlertCallCount = 0;
+        ledAlertCallCount = 0;
+    }
 }
